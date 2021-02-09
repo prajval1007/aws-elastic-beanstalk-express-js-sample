4f450a19fac5bbd18588f7fbfb6ecc89e20be875
Prajval Shende <prajval.shende@gmail.com>
2021-02-09 14:42:48 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This Project is for DevOps case study!'));
+app.get('/', (req, res) => res.send('This Project is for DevOps!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
